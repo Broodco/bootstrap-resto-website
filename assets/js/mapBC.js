bcd2e6b836312c66dedd2a8be54dec3fdbafef01
Broodco <broodcoorens.maxime@gmail.com>
2019-03-01 10:46:55 +0100

    contact page added and favicon implemented

--- assets/js/mapBC.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    // The location of BeCentral
-    let beCentral = {lat: 50.845499, lng:4.357356};
-    // The map, centered at BeCentral
-    let map = new google.maps.Map(
-        document.querySelector('#mapCent'), {zoom: 16, center: beCentral});
-    // The marker, positioned at BeCentral
-    let marker = new google.maps.Marker({position: beCentral, map: map});
-}
